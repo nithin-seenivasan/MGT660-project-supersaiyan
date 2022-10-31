4d01ae6764b37a14c99f2bb5ed347f755bfe9008
mehamp94 <112638607+mehamp94@users.noreply.github.com>
2022-10-31 11:10:25 -0400

    Attempt at building webpage framework
    Added controllers and relationships for about page, creating event page, and events page

--- index_controllers.go
+++ index_controllers.go
@@ -25,3 +25,40 @@ func indexController(w http.ResponseWriter, r *http.Request) {
 
 	tmpl["index"].Execute(w, contextData)
 }
+func aboutController(w http.ResponseWriter, r *http.Request) {
+
+	type indexContextData struct {
+		Events []Event
+		Today  time.Time
+	}
+
+	theEvents, err := getAllEvents()
+	if err != nil {
+		http.Error(w, "database error", http.StatusInternalServerError)
+		return
+	}
+
+	contextData := indexContextData{
+		Events: theEvents,
+		Today:  time.Now(),
+	}
+	tmpl["about"].Execute(w, contextData)
+}
+func createController(w http.ResponseWriter, r *http.Request) {
+	type indexContextData struct {
+		Events []Event
+		Today  time.Time
+	}
+
+	theEvents, err := getAllEvents()
+	if err != nil {
+		http.Error(w, "database error", http.StatusInternalServerError)
+		return
+	}
+
+	contextData := indexContextData{
+		Events: theEvents,
+		Today:  time.Now(),
+	}
+	tmpl["createevent"].Execute(w, contextData)
+}
