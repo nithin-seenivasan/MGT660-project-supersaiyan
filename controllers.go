8d487796adc392c0013f01e777d17c30f83ecbae
Nithin Seenivasan <76654861+nithin-seenivasan@users.noreply.github.com>
2022-11-06 22:36:22 -0500

    Bug fixes
    Removed table drop, removed bunch of println used for debugging

--- controllers.go
+++ controllers.go
@@ -54,6 +54,8 @@ func addNewEventToDb(w http.ResponseWriter, r *http.Request) {
 	location := r.FormValue("location")
 	image := r.FormValue("image")
 	date := r.FormValue("date")
+
+	//Create a Error Message based on conditions
 	var errorMessage string
 	if len(title) < 2 {
 		errorMessage = "Title is invalid!"
@@ -63,15 +65,12 @@ func addNewEventToDb(w http.ResponseWriter, r *http.Request) {
 		errorMessage = errorMessage + " Location is invalid!"
 	}
 
-	//Do regex for Image
+	//Use DB "CHECK" statement for image URL
 
 	//Parses date string to time.Time element
 	parsedDate, err := time.Parse("2006-01-02T15:04", date)
 	if err != nil {
 		errorMessage = errorMessage + " Date is invalid!"
-		println("Parsed Date Error Handler")
-		println(errorMessage)
-		println(err.Error())
 	}
 
 	//Create a Event element with the new variables
@@ -82,21 +81,21 @@ func addNewEventToDb(w http.ResponseWriter, r *http.Request) {
 		Date:     parsedDate,
 	}
 
+	//If no Error, execute this -
 	if errorMessage == "" {
 		//Insert into Postgres DB.
 		newID, err := addEvent(newEvent)
 		if err != nil {
-			//Error here should be injected into a reloaded form.
+			//Error here comes from the INSERT SQL statement - display the following message
 			tmpl["create"].Execute(w, "Unable to accept input. Please check the entered data. Note: only png|jpg|jpeg|gif|gifv images are supported")
-			println(err.Error())
 			return
 		}
 		tmpl["post-creation"].Execute(w, newID)
 		return
 	}
 	errorMessage = errorMessage + " Please try again!"
-	tmpl["create"].Execute(w, errorMessage)
 
-	//If all else is good, the Event with ID = newID is displayed
+	//Display the create page with the concatenated error Message (containing aggregate of all error messages)
+	tmpl["create"].Execute(w, errorMessage)
 
 }
